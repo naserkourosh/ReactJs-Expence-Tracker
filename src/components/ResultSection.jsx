76c7f0becb01b59118c22ea23c0a4a5e5c5fe1f7
Naser Kourosh <naserkorosh1986@gmail.com>
2021-10-22 22:13:24 +0330

    colorized result section

--- src/components/ResultSection.jsx
+++ src/components/ResultSection.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import TransactionButton from './TransactionButton';
 
-export default function ResultSection() {
+export default function ResultSection({order='01',category='Income', amount=0}) {
   return (
     <>
     <TransactionButton/>
-      <div className="daily">{new Date().getFullYear()}</div>
+      <div className="daily">{new Date().toLocaleString('en-us', { month: 'short' })} {new Date().getFullYear()}</div>
       <div className="result">
         <div className="description">
           <div className="order">01</div>
           <div className="divider"></div>
           <div className="name-description">
-            <span className="category">Income</span>
+            <span className="category">{category}</span>
             <small className="name">Salary</small>
           </div>
         </div>
         <div className="saving">
-          <span className="green">+$2,800.00</span>
+        {category==='Income' ?<span className="green">+${amount}</span> : <span className="red">-${amount}</span> }
+          
+        </div>
+      </div>
+      <div className="result">
+        <div className="description">
+          <div className="order">{order='02'}</div>
+          <div className="divider"></div>
+          <div className="name-description">
+            <span className="category">{category="Expence"}</span>
+            <small className="name">Salary</small>
+          </div>
+        </div>
+        <div className="saving">
+        {category==='Income' ?<span className="green">+${amount}</span> : <span className="red">-${amount}</span> }
+          
         </div>
       </div>
     </>
